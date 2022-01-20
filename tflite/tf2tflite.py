63a4463eafd7143443c427f4ece833be8b7185e4
Subin <39542978+subeans@users.noreply.github.com>
2022-01-20 16:45:55 +0900

    tf->tflite->ONNX

---
+++ tflite/tf2tflite.py
@@ -0,0 +1,29 @@
+# how to run : example
+# python3 tf2tflite.py --model mobilenet --model_type classification
+
+import tensorflow as tf
+
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--model',default='resnet50' , type=str)
+parser.add_argument('--model_type',default='classification' , type=str)
+
+args = parser.parse_args()
+model_name = args.model
+model_type = args.model_type
+
+saved_model_dir = f"../tensorflow/{model_type}/{model_name}_saved_model"
+
+# Convert the model
+converter = tf.lite.TFLiteConverter.from_saved_model(saved_model_dir) # path to the SavedModel directory
+converter.target_spec.supported_ops = [
+  tf.lite.OpsSet.TFLITE_BUILTINS, # enable TensorFlow Lite ops.
+  tf.lite.OpsSet.SELECT_TF_OPS # enable TensorFlow ops.
+]
+tflite_model = converter.convert()
+
+# Save the model.
+with open(f'./{model_type}/{model_name}.tflite', 'wb') as f:
+  f.write(tflite_model)
+print("TF to TFLITE convert done")
---
+++ tflite/tflite2onnx.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+printf "tflite2onnx convert start"
+
+MODEL_NAME=$1
+MODEL_TYPE=$2
+
+pip install -U tf2onnx
+
+python -m tf2onnx.convert \
+        --opset 9 \
+        --tflite ${MODEL_TYPE}/${MODEL_NAME}.tflite\
+        --output ../onnx/${MODEL_NAME}_tflite.onnx \
+        
+
+
+printf "convert done"
