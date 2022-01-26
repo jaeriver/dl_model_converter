9a5f450934749da99b5ca9e90765d61703f71db0
최재강 <chl8273@naver.com>
2022-01-26 17:18:51 +0900

    Update inference.py

--- onnx/inference.py
+++ onnx/inference.py
@@ -1,1 +1,32 @@
-init
+import os
+import numpy as np
+import onnxruntime as rt
+import tf2onnx
+from transformers import BertTokenizer, TFBertModel
+import time
+
+tf_model_path = 'tf/bert'    
+
+tokenizer = BertTokenizer.from_pretrained(tf_model_path, cache_dir=None, local_files_only=True)
+model = TFBertModel.from_pretrained(tf_model_path, cache_dir=None, local_files_only=True)
+
+# switch the input_dict to numpy
+sentence = "This is Fake Dataset for testing NLP Tokenizing"
+test_batch = [sentence for i in range(batch_size)]
+encoded_input = tokenizer(test_batch, 
+                          return_tensors='tf',
+                          padding=True,
+                          truncation=True,
+                          max_length=512)
+input_dict_np = {k: v.numpy() for k, v in encoded_input.items()}
+output_names = list(tf_results.keys())
+
+tf_results = model(encoded_input)
+
+model_name = "bert.onnx"
+opt = rt.SessionOptions()
+sess = rt.InferenceSession(model_name)
+start_time = time.time()
+onnx_results = sess.run(output_names, input_dict_np)
+print(time.time() - start_time)
+print(onnx_results)
