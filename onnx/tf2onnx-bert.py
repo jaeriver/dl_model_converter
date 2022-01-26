621c88febedf2ee5fccd7c7cac48dc5d8d4ce9a5
최재강 <chl8273@naver.com>
2022-01-26 16:55:56 +0900

    Update tf2onnx-bert.py

--- onnx/tf2onnx-bert.py
+++ onnx/tf2onnx-bert.py
@@ -26,7 +26,7 @@ def get_model(model_name, bucket_name):
             continue;
         bucket.download_file(object.key, object.key)
     
-    return model_name
+    return 'tf/' + model_name
  
 model_path = get_model(model_name, bucket_name)
 
